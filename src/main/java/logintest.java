59d37ebf97e8823ccae9d821aac9c5f265198d89
Barkomaxno <k.n.ivanov@mail.ru>
2019-11-27 21:24:53 +0300

    Тест

--- src/main/java/logintest.java
+++ src/main/java/logintest.java
@@ -1,13 +1,9 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
